bcf1826a819841788eb80e467bd6d01c1d698b7d
Andrey Sobol <andrey.sobol@bpmobile.com>
2019-04-22 18:44:28 +0300

    Added 404-page template

--- 404.php
+++ 404.php
@@ -1,5 +1,11 @@
-<?php get_header(); ?>
-
-<h1>There is no page!</h1>
+<?php get_header('page'); ?>
+<main>
+	<section class="section main-block">
+		<div class="wrapper flex-wrapper">
+			<div class="section__title"><span class="text text_header-2 text_light text_capit text_black">There is no page!</span></div>
+			<div class="section__text"><span class="text">You must to look for something else.</span></div>
+		</div>
+	</section>
+</main>
 
 <?php get_footer(); ?>
