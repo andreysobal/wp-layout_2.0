e5a79168403b285a466614bb21439feb72c4e87c
Andrey Sobol <andrey.sobol@bpmobile.com>
2019-05-27 18:49:06 +0300

    Added sidebar to 404-page. Fixed sidebar width.

--- 404.php
+++ 404.php
@@ -1,9 +1,12 @@
 <?php get_header('page'); ?>
 <main>
 	<section class="section main-block">
-		<div class="wrapper flex-wrapper">
-			<div class="section__title"><span class="text text_header-2 text_light text_capit text_black">There is no page!</span></div>
-			<div class="section__text"><span class="text">You must to look for something else.</span></div>
+		<div class="wrapper flex-wrapper flex-wrapper_nowrap flex-wrapper_align-start">
+			<div class="section__content content">
+				<div class="section__title"><span class="text text_header-2 text_light text_capit text_black">There is no page!</span></div>
+				<div class="section__text"><span class="text">You must to look for something else.</span></div>
+			</div>
+			<?php get_sidebar(); ?>
 		</div>
 	</section>
 </main>
