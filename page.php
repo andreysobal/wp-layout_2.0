25dca14ee5206f61ef7ed1901f1f048d9eb26164
andreysobal <andrey.sobol@bpmobile.com>
2019-07-24 17:06:12 +0300

    Added single page template file

---
+++ page.php
@@ -0,0 +1,20 @@
+<?php get_header('page'); ?>
+
+<main>
+	<section class="section">
+		<div class="activities__item main-block">
+			<div class="wrapper flex-wrapper">
+				<div class="flex-wrapper__img illustration">Flow Tree Icon</div>
+				<div class="flex-wrapper__content">
+					<div class="section__title text text_header-2 text_light text_capit text_black">Web <b>Development</b></div>
+					<div class="section__text">
+						<p class="text text_separated text_low">Proin iaculis purus consequat sem cure digni ssim. Donec porttitora entum suscipit aenean rhoncus posuere odio in tincidunt. Proin iaculis purus consequat sem cure digni ssim. Donec porttitora entum suscipit aenean rhoncus posuere odio in tincidunt. </p>
+						<p class="text text_low">Iaculis purus consequat sem cure digni ssim. Donec porttitora entum suscipit aenean rhoncus posuere odio in tincidunt. Proin iaculis purus consequat sem cure digni ssim.</p>
+					</div>
+				</div>
+			</div>
+		</div>
+	</section>
+</main>
+
+<?php get_footer(); ?>
