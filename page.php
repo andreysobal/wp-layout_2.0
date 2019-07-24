d346a26b4efcc16ae317934b42fc6224ce57a86f
@andreysobal <andrey.sobol@bpmobile.com>
2019-07-24 17:34:34 +0300

    Fexed username in gitconfig

--- page.php
+++ page.php
@@ -7,12 +7,7 @@
 				<div class="flex-wrapper__img illustration">Flow Tree Icon</div>
 				<div class="flex-wrapper__content">
 					<div class="section__title text text_header-2 text_light text_capit text_black"><?php the_title(); ?></div>
-					<div class="section__text">
-						<?php 
-						the_post();
-						the_content(); 
-						?>
-					</div>
+					<div class="section__text"> <?php  the_post(); the_content(); ?> </div>
 				</div>
 			</div>
 		</div>
